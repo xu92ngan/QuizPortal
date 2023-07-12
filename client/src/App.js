e733251db95359b5c5964f166e89070e14a5c65e
trungnq <trung.q.nguyen@gbst.com>
2023-07-12 13:56:37 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './stylesheets/alignments.css';
+import './stylesheets/textelements.css';
+import './stylesheets/theme.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='h-screen bg-primary'></div>
+    </div>
+  );
+}
+
+export default App;
